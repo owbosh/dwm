50c039ee1f9061c6c3c14dd5318232b95c1f1ac6
owbosh <albertoshea2@gmail.com>
2021-12-10 18:44:01 +1100

    center floating

--- dwm.c
+++ dwm.c
@@ -1395,6 +1395,11 @@ manage(Window w, XWindowAttributes *wa)
 	updatewindowtype(c);
 	updatesizehints(c);
 	updatewmhints(c);
+	
+	/* center floating */
+	c->x = c->mon->mx + (c->mon->mw - WIDTH(c)) / 2;
+	c->y = c->mon->my + (c->mon->mh - HEIGHT(c)) / 2;
+
 	XSelectInput(dpy, w, EnterWindowMask|FocusChangeMask|PropertyChangeMask|StructureNotifyMask);
 	grabbuttons(c, 0);
 	if (!c->isfloating)
